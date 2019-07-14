17ab127ebdf22fd3a2a732c7ccfc0229870912cf
Bùi Văn Phong <phongproptit@outlook.com>
2019-07-14 21:01:15 +0700

    First commit

---
+++ Example.Fody/Example.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Example.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(Add(2, 4,3));
+            ReadLine();
+        }
+
+        private static int Add(int a, int b, int c)
+        {
+            return a + b;
+        }
+    }
+}
